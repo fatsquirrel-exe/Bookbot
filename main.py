51a46d572677145f460e80813643981334476106
Fat Squirrel IPad <fatsquirrel.exe@gmail.com>
2024-04-08 13:30:46 +0800

    Added letter count sorted by descending order

--- main.py
+++ main.py
@@ -3,9 +3,39 @@ dict = {} #do not remove, used for initialise_dict()
 def main():
     book_path = "books/frankenstein.txt"
     text = get_text(book_path)
-    print(f"Num of words: {wordcount(text)}")
     initialise_dict(text)
-    print(charCount(text))
+    charCount(text)
+    processed_dict = process_dict()
+    processed_dict.sort(reverse=True, key=call_value)
+    #key is callable object, inserts each item in process_dict
+    #iterating through each item through the list
+    #due to sort, then list is arranged according to values from call_value()
+    
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{wordcount(text)} words found in the document")
+    print("")
+    for dictionary in processed_dict:
+        name = dictionary["name"]
+        num = dictionary["num"]
+        if name.isalpha():
+            print(f"The {name} character was found {num} times")
+    print("")
+    print("--- End Report ---")
+
+
+
+
+
+def process_dict():
+    list_dict = []
+    for i in dict:
+        temp_dict = {"name":i, "num":dict[i]}
+        list_dict.append(temp_dict)
+    return list_dict
+
+def call_value(dict):
+    return dict["num"]
 
 def get_text(book_path):
     with open(book_path) as f:
